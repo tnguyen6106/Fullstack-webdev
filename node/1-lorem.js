5caf7384125d1b2b21d0ace86d5f123528c22e27
sonynguyen <thon@pdx.edu>
2019-04-23 13:22:34 -0700

    Finish 1st node exercise

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -2,4 +2,20 @@
 
 var http = require('http'); // do not change this line
 
+//Hello world program starts here
+var server = http.createServer(function(req, res) {
+    //console.log("console logging");
+    //console.log(req.url);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+    res.end();
+});
+
+//console.log('sever listening on port 8080');
+server.listen(process.env.PORT||8080);
+//====================================
+
+//set('port', process.env.PORT||8080);
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
