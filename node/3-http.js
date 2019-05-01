5f8ba39b8b6bbf275b34f5000c2b49b2f71a6307
sonynguyen <thon@pdx.edu>
2019-04-30 20:20:55 -0700

    Finish 3rd exercise node.js

--- node/3-http.js
+++ node/3-http.js
@@ -2,6 +2,10 @@
 
 var http = require('http'); // do not change this line
 
+// req.headers.cookie
+const server = http.createServer(function(req, res){
+
+
 // http://localhost:8080/missing should return a status code 404 with 'your princess is in another castle' in plain text
 
 // http://localhost:8080/redirect should redirect the request to '/redirected' by using 302 as the status code
@@ -11,3 +15,60 @@ var http = require('http'); // do not change this line
 // http://localhost:8080/cookie should return 'i gave you a cookie' in plain text and set 'hello=world' as a cookie
 
 // http://localhost:8080/check should return 'yes' / 'no' in plain text depending on whether the browser has the 'hello' cookie
+  // if (req.url === '/') {
+  //     res.writeHead(404, {
+  //         'Content-Type': 'text/plain'
+  //     });
+  //     res.write('you have accessed the root');
+  //     res.end();
+    //const currentURL = require('querystring').parse(req.url);
+    console.log(req.url);
+    if (req.url.indexOf('/missing') === 0) {
+         res.writeHead(404, {
+              'Content-Type': 'text/plain'
+         });
+         res.write('your princess is in another castle');
+         res.end();
+    } else if(req.url === '/redirect') {
+        res.writeHead(302, {
+            'Content-Type' : 'text/plain',
+            'Location': '/redirected'
+        });
+        res.end();
+    } else if(req.url === '/cache') {
+        res.writeHead(200, {
+            'Content-Type' : 'text/plain',
+            'Cache-Control' : 'max-age=86400'
+        });
+        res.write('cache this resource');
+        res.end();
+    } else if(req.url === '/cookie') {
+        res.writeHead(200, {
+            'Content-Type' : 'text/plain',
+            'Set-Cookie' : 'hello=world'
+        });
+        res.write('i gave you a cookie');
+        res.end();
+    } else if(req.url === '/check') {
+        var cookieString = req.headers.cookie;
+        var helloRegex = /hello=/;
+
+        res.writeHead(200, {
+            'Content-Type' : 'text/plain',
+        });
+        
+        if(helloRegex.test(cookieString) === true) {
+            res.write('yes');
+        } else {
+            res.write('no');
+        }
+        res.end();
+    }
+    else {
+        console.log(res.statusCode);
+        res.end();
+    }
+});
+
+console.log('server listerning on port 8080');
+server.listen(process.env.PORT || 8080);
