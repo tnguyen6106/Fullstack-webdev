155bea7297c0fa9e4c8493c47a52afe09eba8a4e
sonynguyen <thon@pdx.edu>
2019-05-11 20:56:22 -0700

    Finish exercise 5 and 6

--- express/5-form.js
+++ express/5-form.js
@@ -3,8 +3,53 @@
 var express = require('express'); // do not change this line
 var parser = require('body-parser'); // do not change this line
 
+let newMessage = '';
 // preface: use the body-parser middleware that helps you retrieve and parse the post data from the form
+var server = express();
+server.use(parser.urlencoded())
 
+server.get('/form', function(req, res) {
+    res.status(200);
+    res.set({
+    	'Content-Type': 'text/html'
+    });
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+    	res.write('<body>');
+    		res.write('<form action="/new" method="post">');
+    			res.write('<input type="text" name="name">');
+    			res.write('<input type="text" name="message">');
+    			res.write('<input type="submit" value="submit">');
+    		res.write('</form>');
+    	res.write('</body>');
+    res.write('</html>');
+    res.end(); 
+});
+
+server.post('/new', function(req, res) {
+    console.log(req.body);
+    if(newMessage === '') {
+        newMessage += req.body.name + ': ' + req.body.message;
+    } else {
+        newMessage += '\n' + req.body.name + ': ' + req.body.message;
+    }
+    console.log(newMessage);
+    res.set({
+        'Content-Type': 'text/plain'
+    });
+    res.write('thank you for your message');
+    res.end();
+});
+
+server.get('/list', function(req, res) {
+    res.set({
+        'Content-Type': 'text/plain'
+    });
+    res.write(newMessage);
+    res.end();
+});
+console.log('serving on port 8080');
+server.listen(process.env.PORT || 8080);
 // http://localhost:8080/form should return the form as shown below
 //   res.status(200);
 //   
--- express/6-auth.js
+++ express/6-auth.js
@@ -4,8 +4,54 @@ var express = require('express'); // do not change this line
 var passport = require('passport'); // do not change this line
 var strategy = require('passport-http'); // do not change this line
 
-// preface: use the passport middleware and only grant the user "test" with the password "logmein" access
 
+var basicStrategy = require('passport-http').BasicStrategy
+
+const server = express();
+// preface: use the passport middleware and only grant the user "test" with the password "logmein" access
+// Reference from https://stackoverflow.com/questions/34378937/basic-auth-with-passport-and-express
+server.use(passport.initialize()); // May need to initialize it or you may not
+const users = {'test': 'logmein'}
+passport.use(new basicStrategy(
+    function(username, password, done) {
+        if (!users[username] || users[username] != password) {
+            return done (null, false);
+        }
+        return done(null, {username: username});
+    }
+));
+
+server.get('/hello', function(req, res) {
+    res.set({
+        'Content-Type': 'text/plain'
+    });
+    res.write('accessible to everyone');
+    res.end();
+});
+
+server.get('/world', 
+    passport.authenticate('basic', { session: false}),
+    function(req, res) {
+        res.set({
+            'Content-Type': 'text/plain'
+        });
+        res.write('only accessible when logged in');
+        res.end();
+    }
+);
+
+server.get('/test', 
+    passport.authenticate('basic', { session: false}),
+    function(req, res) {
+        res.set({
+            'Content-Type': 'text/plain'
+        });
+        res.write('only accessible when logged in');
+        res.end();
+    }
+);
+console.log('serving on port 8080');
+server.listen(process.env.PORT || 8080);
 // note: should the server restart, the browser will still technically be logged in since we are using the http basic access authentication which lets the browser submit the username and the password at each request
 
 // http://localhost:8080/hello should return 'accessible to everyone' in plain text
